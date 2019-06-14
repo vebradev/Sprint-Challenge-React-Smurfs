6b93e5cd40b7f543b00459b9b5a82a1b47e0e6b6
vebradev <justinas@vebra.dev>
2019-06-14 11:49:16 +0300

    Construct axios post request to add new Smurf.

--- village/src/App.js
+++ village/src/App.js
@@ -6,12 +6,18 @@ import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
 
 const data = "http://localhost:3333/smurfs";
+const smurfData = {
+  name: "",
+  age: "",
+  height: ""
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      smurf: smurfData,
       errMsg: null
     };
   }
@@ -26,12 +32,37 @@ class App extends Component {
       .then(res => this.setState({ smurfs: res.data }))
       .catch(err => this.setState({ errMsg: err.message }))
       .finally(console.log(data));
-  }
+  };
+
+  handleInput = e => {
+    e.persist();
+    this.setState(prevState => {
+      return {
+        smurf: {
+          ...prevState.smurf,
+          [e.target.name]: e.target.value
+        }
+      };
+    });
+  };
+
+  addData = () => {
+    console.log("add happening");
+    const newSmurf = this.state.smurf;
+
+    axios
+      .post(data, newSmurf)
+      .then(() => this.fetchData());
+  };
 
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm
+          smurf={this.state.smurf}
+          handleInput={this.handleInput}
+          addData={this.addData}
+        />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
