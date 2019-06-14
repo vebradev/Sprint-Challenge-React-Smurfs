656bac3d84ff5eb954a15fd6712f2e4f5852d65f
vebradev <justinas@vebra.dev>
2019-06-14 11:49:00 +0300

    Construct axios post request to add new Smurf.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,57 +1,17 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-  }
-
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
-
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
-          <button type="submit">Add to the village</button>
-        </form>
-      </div>
-    );
-  }
+function SmurfForm(props) {
+  return (
+    <div className="SmurfForm">
+      <input type="text" placeholder="Name" name="name" onChange={props.handleInput} />
+      <br />
+      <input type="text" placeholder="Age" name="age" onChange={props.handleInput} />
+      <br />
+      <input type="text" placeholder="Height" name="height" onChange={props.handleInput} />
+      <br />
+      <button onClick={props.addData}>Add to the village</button>
+    </div>
+  );
 }
 
 export default SmurfForm;
