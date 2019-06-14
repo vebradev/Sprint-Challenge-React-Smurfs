dcce775be6c9657d94a36bd1194a7b99d0124810
vebradev <justinas@vebra.dev>
2019-06-14 10:22:01 +0300

    Retrieve data from server.

--- village/src/App.js
+++ village/src/App.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+
+const data = "http://localhost:3333/smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      errMsg: null
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    axios
+      .get(data)
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => this.setState({ errMsg: err.message }))
+      .finally(console.log(data));
+  }
+
   render() {
     return (
       <div className="App">
